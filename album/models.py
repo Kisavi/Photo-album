ccc81cccdfc0b8525880c6ac53fd4699cea85ad7
Kisavi <deniskisavi@gmail.com>
2022-05-26 20:43:58 +0300

    register our models in our admin to create there interface in the admin panel

--- album/models.py
+++ album/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100, null=False, blank=False)
+
+
+class Picture(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    pub_date = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(null=False, blank=False)
+    description = models.TextField(max_length=500, null=False, blank=False)
--- album/urls.py
+++ album/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.gallery),
-    path('photo/<str:pk>/', views.viewPhoto),
-    path('add/', views.uploadPhoto),
+    path('', views.gallery, name='gallery'),
+    path('photo/<str:pk>/', views.viewPhoto, name='photo'),
+    path('add/', views.uploadPhoto, name='upload'),
 ]
