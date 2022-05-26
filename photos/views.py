4accefc0e55381427d030d5ae8bf1c2f760f102b
Kisavi <deniskisavi@gmail.com>
2022-05-26 14:30:24 +0300

    create differnt views which are to be mapped in the urls

---
+++ photos/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+
+# Create your views here.
+def gallery(request):
+    return render(request, 'photos/templates/gallery.html')
+
+
+def viewPhoto(request):
+    return render(request, 'photos/templates/photo.html')
+
+
+def uploadPhoto(request):
+    return render(request, 'photos/templates/upload.html')
