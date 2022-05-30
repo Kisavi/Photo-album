149a27a933695ccc30714d5839e94e08f835539b
Kisavi <deniskisavi@gmail.com>
2022-05-30 13:21:21 +0300

    set debug mode to false

--- photoshare/settings.py
+++ photoshare/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=)q5=be)&8fi$t7&b35(i31*cyq&z$rrc2a_)lo&ujo&!@)0h!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['picxels.herokuapp.com', '127.0.0.1']
 
