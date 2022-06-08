7b174bcdd041551acae397f407b3f4cc10320cee
Kisavi <deniskisavi@gmail.com>
2022-06-08 22:12:43 +0300

    add unittests for the models and the models methods

--- album/models.py
+++ album/models.py
@@ -5,12 +5,46 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=100, null=False, blank=False)
 
+    def save_category(self):
+        self.save()
+
+    @classmethod
+    def get_category_by_id(cls, id):
+        category = cls.objects.get(id=id)
+        return category
+
+    @classmethod
+    def update_category(cls, cat):
+        category = cls.get_category_by_id(cat.id)
+        category.name = cat.name
+        category.save_category()
+
+    def delete_category(self):
+        self.delete()
+
     def __str__(self):
         return self.name
 
 
 class Location(models.Model):
     name = models.CharField(max_length=100, null=False, blank=False)
+
+    def save_location(self):
+        self.save()
+
+    @classmethod
+    def get_location_by_id(cls, id):
+        location = cls.objects.get(id=id)
+        return location
+
+    @classmethod
+    def update_location(cls, loc):
+        location = cls.get_location_by_id(loc.id)
+        location.name = loc.name
+        location.save_location()
+
+    def delete_location(self):
+        self.delete()
 
     def __str__(self):
         return self.name
@@ -25,7 +59,11 @@ class Picture(models.Model):
 
     def save_picture(self):
         self.save()
-        return self
+
+    @classmethod
+    def get_picture_by_id(cls, id):
+        picture = cls.objects.get(id=id)
+        return picture
 
     @classmethod
     def update_picture(cls, pic):
@@ -40,16 +78,14 @@ class Picture(models.Model):
         self.delete()
 
     @classmethod
-    def get_image_by_id(cls, id):
-        image = cls.objects.get(id=id)
-        return image
-
-    @classmethod
-    def search_image(cls, category):
-        images = cls.objects.filter(category__name__icontains=category)
-        return images
+    def search_picture(cls, category):
+        pictures = cls.objects.filter(category__name__icontains=category)
+        return pictures
 
     @classmethod
     def filter_by_location(cls, location):
-        images = cls.objects.filter(location__name__icontains=location)
-        return images
+        pictures = cls.objects.filter(location__name__icontains=location)
+        return pictures
+
+    def __str__(self):
+        return self.category
--- album/tests.py
+++ album/tests.py
@@ -1,3 +1,144 @@
 from django.test import TestCase
+from .models import Picture, Location, Category
+
 
 # Create your tests here.
+
+class CategoryTestCase(TestCase):
+    """Test case for Category model"""
+
+    def setUp(self):
+        """Set up for the models instances"""
+        self.category = Category(name='Nature')
+
+    def tearDown(self):
+        """Method to clear the model objects in the db after each testcase"""
+        Category.objects.all().delete()
+
+    def test_instance(self):
+        """Test case to check if created category is an instance of Category model class"""
+        self.assertTrue(isinstance(self.category, Category))
+
+    def test_save_category(self):
+        """Test case to check if created category is being saved in our database"""
+        self.category.save_category()
+        categories = Category.objects.all()
+        self.assertTrue(len(categories) > 0)
+
+    def test_update_category(self):
+        """Test case to check if a saved category is being updated successfully"""
+        self.category.save_category()
+        self.category.name = 'Food'
+        Category.update_category(self.category)
+        updated_category = Category.objects.get(id=self.category.id)
+        self.assertEqual(updated_category.name, 'Food')
+
+    def test_delete_category(self):
+        """Test case to check if a saved category is being removed from our db when deleted."""
+        self.category.save_category()
+        test_category = Category(name='Travel')
+        test_category.save_category()
+        self.category.delete_category()
+        categories = Category.objects.all()
+        self.assertTrue(len(categories), 1)
+
+
+class LocationTestCase(TestCase):
+    """Test case for Location model"""
+
+    def setUp(self):
+        """Set up for the models instances"""
+        self.location = Location(name='Doha')
+
+    def tearDown(self):
+        """Method to clear the model objects in the db after each testcase"""
+        Location.objects.all().delete()
+
+    def test_instance(self):
+        """Test case to check if created location is an instance of Location model class"""
+        self.assertTrue(isinstance(self.location, Location))
+
+    def test_save_location(self):
+        """Test case to check if created location is being saved in our database"""
+        self.location.save_location()
+        locations = Location.objects.all()
+        self.assertTrue(len(locations) > 0)
+
+    def test_update_location(self):
+        """Test case to check if a saved location is being updated successfully"""
+        self.location.save_location()
+        self.location.name = 'Wuhan'
+        Location.update_location(self.location)
+        updated_location = Location.objects.get(id=self.location.id)
+        self.assertEqual(updated_location.name, 'Wuhan')
+
+    def test_delete_location(self):
+        """Test case to check if a saved location is being removed from our db when deleted."""
+        self.location.save_location()
+        test_location = Location(name='Cairo')
+        test_location.save_location()
+        self.location.delete_location()
+        locations = Location.objects.all()
+        self.assertTrue(len(locations), 1)
+
+
+class PictureTestCase(TestCase):
+    """Test case for Picture model"""
+
+    def setUp(self):
+        """Set up for the models instances"""
+        self.category = Category(name='Nature')
+        self.category.save()
+        self.location = Location(name='Munich')
+        self.location.save()
+        self.picture = Picture(image='test.png', description='Image description', pub_date='2022-02-02 12:22:10',
+                               category=self.category, location=self.location)
+
+    def tearDown(self):
+        """Method to clear the model objects in the db after each testcase"""
+        Category.objects.all().delete()
+        Location.objects.all().delete()
+        Picture.objects.all().delete()
+
+    def test_instance(self):
+        """Test case to check if created picture is instance of Picture model class"""
+        self.assertTrue(isinstance(self.picture, Picture))
+
+    def test_save_picture(self):
+        """Test case to check if created picture is being saved in our database"""
+        self.picture.save_picture()
+        pictures = Picture.objects.all()
+        self.assertTrue(len(pictures) > 0)
+
+    def test_update_picture(self):
+        """Test case to check if a saved picture is being updated successfully"""
+        self.picture.save_picture()
+        self.picture.description = 'New description'
+        Picture.update_picture(self.picture)
+        updated_picture = Picture.objects.get(id=self.picture.id)
+        self.assertEqual(updated_picture.description, 'New description')
+
+    def test_delete_picture(self):
+        """Test case to check if a saved picture is being removed from our db when deleted."""
+        self.picture.save_picture()
+        self.picture.delete_picture()
+        pictures = Picture.objects.all()
+        self.assertTrue(len(pictures) == 0)
+
+    def test_get_picture_by_id(self):
+        """Test case to check we can access the id of a saved image in the database."""
+        self.picture.save_picture()
+        picture = Picture.get_picture_by_id(self.picture.id)
+        self.assertTrue(picture)
+
+    def test_search_by_category(self):
+        """Test case to check if we can search an image by category."""
+        self.picture.save_picture()
+        pictures = Picture.search_picture('Nature')
+        self.assertEqual(len(pictures), 1)
+
+    def test_filter_by_location(self):
+        """Test case to check if we can filter an image by their location."""
+        self.picture.save_picture()
+        pictures = Picture.filter_by_location('Munich')
+        self.assertEqual(len(pictures), 1)
